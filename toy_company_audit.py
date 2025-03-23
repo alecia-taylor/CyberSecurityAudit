9f5ce940f441e56f5fea22e56c0e77fccf748306
A. Taylor <alecia.taylor0@gmail.com>
2025-03-23 16:57:55 +0000

    add BotiumToysAudit class with initial asset definitions

---
+++ toy_company_audit.py
@@ -0,0 +1,12 @@
+class BotiumToysAudit:
+     def __init__(self):
+        self.assets = {
+            "on_premises_equipment": 0,
+            "employee_equipment": 0,
+            "storefront_products": 0,
+            "systems_and_services": 0,
+            "internet_access": 0,
+            "internal_network": 0,
+            "data_retention_storage": 0,
+            "legacy_systems": 0
+        }
